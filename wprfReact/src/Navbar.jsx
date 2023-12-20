ef7977506e93e2ab08c73b4af23a155c03fb4bf0
NazihrToure <101570129+NazihrToure@users.noreply.github.com>
2023-12-20 01:38:16 +0100

    With mobile javascript menu

--- wprfReact/src/Navbar.jsx
+++ wprfReact/src/Navbar.jsx
@@ -1,10 +1,17 @@
 import './Navbar.css';
 import logo from './images/icon_accessibility.png';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import RegisterForm from './register/registerform';
 
 export default function App() {
+   
+      const [mobileButtonsVisible, setMobileButtonsVisible] = useState(false);
 
+      const handleCheckboxClick = () => {
+        setMobileButtonsVisible(!mobileButtonsVisible);
+      };
+  
   return (
       <div>
           <div className="nav-container">
@@ -20,12 +27,12 @@ export default function App() {
                       <li><a href="#Cassussen">Cassussen</a></li>
                       <li><a href="#Over ons">Over ons</a></li>
                       <li><a href="#Contact">Contact</a> </li>
-                      <li id="register"><Link to="/register">Registreren</Link></li>
+                      <li id="register"><a><Link to="/register">Registreren</Link></a></li>
                       <li id="login"><a href="#Login">Log in</a></li>
                   </ul>
               </div>
               <div className="mobile-menu-container">
-                  <input className="checkbox" type="checkbox"/>
+                  <input class="checkbox" type="checkbox" id="checkbox"  onChange={handleCheckboxClick} checked={mobileButtonsVisible}/>
                   <div className="hamburger-lines">
                       <span className="line line1"></span>
                       <span className="line line2"></span>
@@ -33,6 +40,27 @@ export default function App() {
                   </div>
               </div>
           </div>
+          {mobileButtonsVisible && (
+          <div className="mobile-buttons-container" id="mobilebuttons">
+              <div className="mobile-buttons">
+                  <ul>
+                      <li><a href="#Actueel">Actueel</a></li>
+                      <li><a href="#Toegankelijkheid">Toegankelijkheid</a></li>
+                      <li><a href="#Hoe wij helpen">Hoe wij helpen</a></li>
+                      <li><a href="#Sectoren">Sectoren</a></li>
+                      <li><a href="#Cassussen">Cassussen</a></li>
+                      <li><a href="#Over ons">Over ons</a></li>
+                      <li><a href="#Contact">Contact</a> </li>
+                  </ul>
+              </div>
+              <div className="register-login-container">
+                  <ul>
+                      <li id="registerMobile"><a><Link to="/register">Registreren</Link></a></li>
+                      <li id="loginMobile"><a href="#Login">Log in</a></li>
+                  </ul>
+              </div>
+              </div>
+              )}
       </div>
   );
 }
