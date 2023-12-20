d88796c2c04a8671811c1f69180ce453f8241b53
Fried Rice <74664530+shemarpawirodihardjo@users.noreply.github.com>
2023-12-20 10:19:00 +0100

    added docker support

---
+++ wprfReact/server.cjs
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.static('dist'));
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
