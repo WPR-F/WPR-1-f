6e986b1613aff2d3644361cba2be157b986234c0
Tristan B <tristan.bliemer@gmail.com>
2024-01-04 18:01:33 +0100

    updates aan profielpagina

--- wprfReact/src/Profiel/ProfielPagina.jsx
+++ wprfReact/src/Profiel/ProfielPagina.jsx
@@ -1,26 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ProfielPagina.css';
 
 function ProfielPagina({ currentUser, setIsLoggedIn, setCurrentUser }) {
-    
+    const [isEditable, setIsEditable] = useState(false);
+    const [showPassword, setShowPassword] = useState(false);
+
     const loguit = () => {
         setIsLoggedIn(false);
         setCurrentUser(null);
         console.log("Uitgelogd");
     }
 
+    const toggleEditable = () => {
+        setIsEditable(!isEditable);
+    }
+
+    const toggleShowPassword = () => {
+        setShowPassword(!showPassword);
+    }
+
     return (
         <div className='blok'>
             {currentUser ? (
-                <div>
-                    <h1>{currentUser.name}</h1>
-                    <p>{currentUser.email}</p>
+                <div className='profiel'>
+                    <h1>Welkom {currentUser.name}!</h1>
+                    <div className='gegevens'>
+                    <h2>Voornaam: {isEditable ? <textarea>{currentUser.name}</textarea> : currentUser.name}</h2>
+                    <h2>Achternaam: {isEditable ? <textarea>{currentUser.lastName}</textarea> : currentUser.lastName}</h2>
+                    <h2>E-mail: {isEditable ? <textarea>{currentUser.email}</textarea> : currentUser.email}</h2>
+                    <h2>Wachtwoord: {isEditable ? <textarea>{showPassword ? currentUser.password : '******'}</textarea> : showPassword ? currentUser.password : '******'}</h2>
+                    </div>
                 </div>
             ) : (
-                <p>Loading...</p>
+                <p>Log eerst in</p>
             )}
 
+            <button className='toonww' onClick={toggleShowPassword}>{showPassword ? 'verstop wachtwoord' : 'Toon wachtwoord'}</button>
+            <div className='onderkant'>
+            <button onClick={toggleEditable}>{isEditable ? 'Opslaan' : 'Bewerk'}</button>
             <button onClick={loguit}>Uitloggen</button>
+            <button>Verwijder account nf</button>
+            <button>Meer nf</button>
+            </div>
         </div>
     );
 }
