efefaf3f5c88259f4abbde3b9f0443173b22b996
NazihrToure <101570129+NazihrToure@users.noreply.github.com>
2024-01-15 14:36:55 +0100

    S

--- wprfAPI/obj/Debug/net8.0/wprfAPI.AssemblyInfo.cs
+++ wprfAPI/obj/Debug/net8.0/wprfAPI.AssemblyInfo.cs
@@ -10,12 +10,11 @@
 using System;
 using System.Reflection;
 
-[assembly: Microsoft.AspNetCore.Identity.UI.UIFrameworkAttribute("Bootstrap5")]
 [assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("5c3732c3-2885-4af0-a20a-579eef25a350")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("wprfAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41dcd3343234bd5a6ecc9ae3465b8636411e76f4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("wprfAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("wprfAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
