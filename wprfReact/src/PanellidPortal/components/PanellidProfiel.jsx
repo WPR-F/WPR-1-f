46b9784588d1df8b15a97a3f4fdf9ceb1a2e98c4
Tristan B <tristan.bliemer@gmail.com>
2024-01-21 12:46:35 +0100

    PanellidProfiel laat nu ook de informatie uit panellid table zien als deze al een keer ingevuld is door de gebruiker

--- wprfReact/src/PanellidPortal/components/PanellidProfiel.jsx
+++ wprfReact/src/PanellidPortal/components/PanellidProfiel.jsx
@@ -9,12 +9,29 @@ const PanellidProfiel = ({ currentUser }) => {
     const [activeGroup, setActiveGroup] = useState('normal');
     const [isEditable, setIsEditable] = useState(false);
     const [selectedDays, setSelectedDays] = useState({});
+    const [panellidinfo, setPanellidinfo] = useState({user: []});
+
+    const fetchUserInformation = async () => {
+
+        try {
+            const response = await fetch('http://localhost:5210/api/Panellid/getPanellidInfo?id=' + currentUser.id);
+            const data = await response.json();
+            setPanellidinfo(data);
+            }
+            catch (error) {
+                console.error('Network error:', error);
+            }
+    }
+
+    useEffect(() => {
+        setPanellidinfo(fetchUserInformation());
+    }, []);
 
     const [form, setForm] = useState({
-        voornaam: currentUser.userName,
-        achternaam: currentUser.lastName,
+        voornaam: currentUser?.userName,
+        achternaam: currentUser?.lastName,
         postcode: '',
-        email: currentUser.email,
+        email: currentUser?.email,
         telefoonnummer: '',
         typeBeperking: '',
         hulpmiddelen: '',
@@ -24,6 +41,23 @@ const PanellidProfiel = ({ currentUser }) => {
         commercielePartijen: 'Ja',
         beschikbaarheid: ''
     });
+
+    useEffect(() => {
+        if (panellidinfo?.panellid) {
+            setForm(prevForm => ({
+                ...prevForm,
+                postcode: panellidinfo.panellid.postalCode,
+                telefoonnummer: panellidinfo.panellid.phoneNumber,
+                typeBeperking: panellidinfo.panellid.disabilityType,
+                hulpmiddelen: panellidinfo.panellid.tools,
+                aandoening: panellidinfo.panellid.condition,
+                typeOnderzoek: panellidinfo.panellid.researchType.split(', '),
+                voorkeurBenadering: panellidinfo.panellid.preferdresearchApproach,
+                commercielePartijen: panellidinfo.panellid.commercialApproach ? 'Ja' : 'Nee',
+                beschikbaarheid: panellidinfo.panellid.availibility
+            }));
+        }
+    }, [panellidinfo]);
 
     const slaGegevensOp = async (event) => {
     
@@ -36,7 +70,7 @@ const PanellidProfiel = ({ currentUser }) => {
             condition: form.aandoening,
             ResearchType: form.typeOnderzoek.join(', '),
             PreferdresearchApproach: form.voorkeurBenadering,
-            CommercialApproach: form.commercielePartijen === 'Ja', // Convert to boolean
+            CommercialApproach: form.commercielePartijen === 'Ja', 
             Availibility: form.beschikbaarheid
         };
         console.log(panellidInfo);
@@ -57,25 +91,27 @@ const PanellidProfiel = ({ currentUser }) => {
         }
     };
 
-    const handleChange = (event) => {
+    const handleTypeOnderzoekChange = (event) => {
+        const { value, checked } = event.target;
+    
+        setForm(prevState => {
+            let newTypeOnderzoek = [...prevState.typeOnderzoek];
+            if (checked) {
+                newTypeOnderzoek.push(value);
+            } else {
+                newTypeOnderzoek = newTypeOnderzoek.filter(item => item !== value);
+            }
+            return { ...prevState, typeOnderzoek: newTypeOnderzoek };
+        });
+    };
+    
+    const handleInputChange = (event) => {
         const { name, value, type, checked } = event.target;
     
-        if (name === 'typeOnderzoek' && type === 'checkbox') {
-            setForm(prevState => {
-                let newTypeOnderzoek = [...prevState.typeOnderzoek];
-                if (checked) {
-                    newTypeOnderzoek.push(value);
-                } else {
-                    newTypeOnderzoek = newTypeOnderzoek.filter(item => item !== value);
-                }
-                return { ...prevState, typeOnderzoek: newTypeOnderzoek };
-            });
-        } else {
-            setForm(prevState => ({
-                ...prevState,
-                [name]: type === 'checkbox' ? (checked ? 'Ja' : 'Nee') : value
-            }));
-        }
+        setForm(prevState => ({
+            ...prevState,
+            [name]: type === 'checkbox' ? (checked ? 'Ja' : 'Nee') : value
+        }));
     };
 
     const handleDayChange = (e) => {
@@ -112,9 +148,6 @@ const PanellidProfiel = ({ currentUser }) => {
         setActiveGroup('onderzoek');
     }
 
-    //Veel te groot component geworden Moet opgesplitst worden in meerdere componenten!!!
-    
-
     return ( 
     <div className="PanellidPortallbg">
         <h1>Profiel</h1>
@@ -125,31 +158,31 @@ const PanellidProfiel = ({ currentUser }) => {
                         <div className="form-group">
                             <label htmlFor="voornaam">Voornaam:</label>
                             <div className="form-input">
-                                <input type="text" id="voornaam" name="voornaam" value={form.voornaam} onChange={handleChange} placeholder="Voornaam" readOnly={!isEditable} />
+                                <input type="text" id="voornaam" name="voornaam" value={form.voornaam} onChange={handleInputChange} placeholder="Voornaam" readOnly={!isEditable} />
                             </div>
                         </div>
                         <div className="form-group">
                             <label htmlFor="achternaam">Achternaam:</label>
                             <div className="form-input">
-                                <input type="text" id="achternaam" name="achternaam" value={form.achternaam} onChange={handleChange} placeholder="Achternaam" readOnly={!isEditable} />
+                                <input type="text" id="achternaam" name="achternaam" value={form.achternaam} onChange={handleInputChange} placeholder="Achternaam" readOnly={!isEditable} />
                             </div>
                         </div>
                         <div className="form-group">
                             <label htmlFor="postcode">Postcode:</label>
                             <div className="form-input">
-                                <input type="text" id="postcode" name="postcode" value={form.postcode} onChange={handleChange} placeholder="Postcode" readOnly={!isEditable} />
+                                <input type="text" id="postcode" name="postcode" value={form.postcode} onChange={handleInputChange} placeholder="Postcode" readOnly={!isEditable} />
                             </div>
                         </div>
                         <div className="form-group">
                             <label htmlFor="email">Email:</label>
                             <div className="form-input">
-                                <input type="text" id="email" name="email" value={form.email} onChange={handleChange} placeholder="Email" readOnly={!isEditable} />
+                                <input type="text" id="email" name="email" value={form.email} onChange={handleInputChange} placeholder="Email" readOnly={!isEditable} />
                             </div>
                         </div>
                         <div className="form-group">
                             <label htmlFor="telefoonnummer">Telefoonnummer:</label>
                             <div className="form-input">
-                                <input type="tel" id="telefoonnummer" name="telefoonnummer" value={form.telefoonnummer} onChange={handleChange} placeholder="Telefoonnummer"  readOnly={!isEditable} />
+                                <input type="tel" id="telefoonnummer" name="telefoonnummer" value={form.telefoonnummer} onChange={handleInputChange} placeholder="Telefoonnummer"  readOnly={!isEditable} />
                             </div>
                         </div>
                     </>
@@ -159,7 +192,7 @@ const PanellidProfiel = ({ currentUser }) => {
                        <div className="form-group">
                            <label htmlFor="typeBeperking">wat voor type beperking heeft u?</label>
                            <div className="form-input">
-                           <select id="typeBeperking" name="typeBeperking" value={form.typeBeperking} onChange={handleChange} disabled={!isEditable}>
+                           <select id="typeBeperking" name="typeBeperking" value={form.typeBeperking} onChange={handleInputChange} disabled={!isEditable}>
                                <option value="">Selecteer type beperking</option>
                                <option value="Motorisch">Motorisch</option>
                                <option value="Zintuigelijk">Zintuigelijk</option>
@@ -170,13 +203,13 @@ const PanellidProfiel = ({ currentUser }) => {
                        <div className="form-group">
                            <label htmlFor="hulpmiddelen">welke hulpmiddelen gebruikt u?</label>
                            <div className="form-input">
-                               <input type="text" id="hulpmiddelen" name="hulpmiddelen" value={form.hulpmiddelen} onChange={handleChange} placeholder="Hulpmiddelen" readOnly={!isEditable}/>
+                               <input type="text" id="hulpmiddelen" name="hulpmiddelen" value={form.hulpmiddelen} onChange={handleInputChange} placeholder="Hulpmiddelen" readOnly={!isEditable}/>
                            </div>
                        </div>
                        <div className="form-group">
                            <label htmlFor="aandoening">Wat voor aandoening heeft u?</label>
                            <div className="form-input">
-                               <input type="text" id="aandoening" name="aandoening" value={form.aandoening} onChange={handleChange} placeholder="Aandoening" readOnly={!isEditable}/>
+                               <input type="text" id="aandoening" name="aandoening" value={form.aandoening} onChange={handleInputChange} placeholder="Aandoening" readOnly={!isEditable}/>
                            </div>
                        </div>
                    </>
@@ -195,7 +228,7 @@ const PanellidProfiel = ({ currentUser }) => {
                                             name="typeOnderzoek"
                                             value="Interview"
                                             checked={form.typeOnderzoek.includes('Interview')}
-                                            onChange={handleChange}
+                                            onChange={handleTypeOnderzoekChange}
                                             aria-label="Interview"
                                         />
                                     </label>
@@ -207,7 +240,7 @@ const PanellidProfiel = ({ currentUser }) => {
                                             name="typeOnderzoek"
                                             value="Groepsgesprekken"
                                             checked={form.typeOnderzoek.includes('Groepsgesprekken')}
-                                            onChange={handleChange}
+                                            onChange={handleTypeOnderzoekChange}
                                             aria-label="Groepsgesprekken"
                                         />
                                     </label>
@@ -219,7 +252,7 @@ const PanellidProfiel = ({ currentUser }) => {
                                             name="typeOnderzoek"
                                             value="Engelstalig onderzoek"
                                             checked={form.typeOnderzoek.includes('Engelstalig onderzoek')}
-                                            onChange={handleChange}
+                                            onChange={handleTypeOnderzoekChange}
                                             aria-label="Engelstalig onderzoek"
                                         />
                                     </label>
@@ -231,7 +264,7 @@ const PanellidProfiel = ({ currentUser }) => {
                                 <div className="form-group">
                                     <label htmlFor="voorkeurBenadering">Voorkeur benadering</label>
                                     <div className="form-input">
-                                    <select id="voorkeurBenadering" name="voorkeurBenadering" value={form.voorkeurBenadering} onChange={handleChange} disabled={!isEditable}>
+                                    <select id="voorkeurBenadering" name="voorkeurBenadering" value={form.voorkeurBenadering} onChange={handleInputChange} disabled={!isEditable}>
                                         <option value="">Selecteer type</option>
                                         <option value="Telefonisch">Telefonisch</option>
                                         <option value="Via Portal">Via Portal</option>
@@ -241,7 +274,7 @@ const PanellidProfiel = ({ currentUser }) => {
                                 <div className="form-group">
                                     <label htmlFor="commercielePartijen">Wilt u deelnamen aan onderzoeken van commerciële partijen?</label>
                                     <div className="form-input">
-                                        <input type="checkbox" id="commercielePartijen" name="commercielePartijen" checked={form.commercielePartijen === 'Ja'} onChange={handleChange} readOnly={!isEditable} />
+                                        <input type="checkbox" id="commercielePartijen" name="commercielePartijen" checked={form.commercielePartijen === 'Ja'} onChange={handleInputChange} readOnly={!isEditable} />
                                     </div>
                                 </div>
                                 <div className="form-group">
