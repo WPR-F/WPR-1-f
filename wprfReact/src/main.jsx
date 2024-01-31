d42d24db9c57d18feef82efa31caa9253b13ce92
Tristan B <tristan.bliemer@gmail.com>
2024-01-31 01:21:50 +0100

    Update imports in main.jsx

--- wprfReact/src/main.jsx
+++ wprfReact/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App.jsx'
-import './index.css'
+import App from './App'
+import './index'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
