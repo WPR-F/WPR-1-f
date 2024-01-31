d8aa3241cbb496d960a308a8985703978b6d407b
Fried Rice <74664530+shemarpawirodihardjo@users.noreply.github.com>
2024-01-31 20:15:12 +0100

    Update wprfAPI version to 1.0.0

--- wprfAPI/obj/Debug/net8.0/wprfAPI.AssemblyInfo.cs
+++ wprfAPI/obj/Debug/net8.0/wprfAPI.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("wprfAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c7dad2e9061418f0627f226aad035ab6b57e488")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("wprfAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("wprfAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
